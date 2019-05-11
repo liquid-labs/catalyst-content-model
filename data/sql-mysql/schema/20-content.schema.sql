74291163b4e721adddd7038bf3c488d44ccf024e
Zane Rockenbaugh <zane@liquid-labs.com>
2019-05-11 17:06:50 -0500

    moved last sync to type text; fixed andadded constraints

--- data/sql-mysql/schema/20-content.schema.sql
+++ data/sql-mysql/schema/20-content.schema.sql
@@ -6,7 +6,6 @@ CREATE TABLE content (
   `namespace` VARCHAR(24),
   `slug` VARCHAR(40),
   `type` ENUM('TEXT') NOT NULL,
-  `last_sync` INT UNSIGNED,
   `version_cookie` VARCHAR(255),
   CONSTRAINT `content_key` PRIMARY KEY ( `id` ),
   CONSTRAINT `content_ref_entities` FOREIGN KEY ( `id` ) REFERENCES `entities` ( `id` )
@@ -14,12 +13,16 @@ CREATE TABLE content (
 
 DELIMITER //
 CREATE TRIGGER `content_slug_constraint`
-  BEFORE INSERT ON contributors FOR EACH ROW
+  BEFORE INSERT ON content FOR EACH ROW
     BEGIN
       IF new.slug REGEXP '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}'
         SIGNAL SQLSTATE '45000'
           SET MESSAGE_TEXT = 'Cannot use slug which may be confused with a UUID.';
       END IF;
+    END;//
+CREATE TRIGGER `content_type_constraint`
+  BEFORE UPDATE ON content FOR EACH ROW
+    BEGIN
       IF new.type != old.type
         SIGNAL SQLSTATE '45000'
           SET MESSAGE_TEXT = 'The content type cannot be changed.';
@@ -32,6 +35,16 @@ CREATE TABLE content_type_text (
   `format` ENUM('TEXT', 'MD', 'HTML', 'CODE'),
   `text` TEXT, -- TODO: in future, possibly expand to 'MEDIUMTEXT', but want to enusre we can enforce size limits to avoid DOS attacks.
                -- The other option would be to keep text and require large works (like a book) to be broken up into multiple content records.
+  `last_sync` INT UNSIGNED,
   CONSTRAINT `content_type_text_key` PRIMARY KEY ( `id` ),
   CONSTRAINT `content_type_text_ref_content` FOREIGN KEY ( `id` ) REFERENCES `content` ( `id` )
 );
+
+CREATE TRIGGER `content_type_text_last_sync_update`
+  BEFORE UPDATE ON content FOR EACH ROW
+    BEGIN
+      IF new.text != old.text
+        SET new.last_sync=UNIX_TIMESTAMP();
+      END IF;
+    END;//
+DELIMITER ;
--- data/sql-mysql/schema/21-contributors.schema.sql
+++ data/sql-mysql/schema/21-contributors.schema.sql
@@ -10,11 +10,18 @@ CREATE TABLE contributors (
 
 DELIMITER //
 CREATE TRIGGER `contributors_credit_order_constraint`
+-- We expect contributers to be cleared and rebuilt
   BEFORE INSERT ON contributors FOR EACH ROW
     BEGIN
       IF new.summary_credit_order <= 0 OR new.summary_credit_order > 3
         SIGNAL SQLSTATE '45000'
           SET MESSAGE_TEXT = 'Summary credit order must be 1, 2, or 3.';
       END IF;
+    END;//
+CREATE TRIGGER `contributors_update_disallowed`
+-- We expect contributers to be cleared and rebuilt
+  BEFORE UPDATE ON contributors FOR EACH ROW
+    SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Invalid update; current model expects contributors to be cleared and refreshed.';
     END;//
 DELIMITER ;
